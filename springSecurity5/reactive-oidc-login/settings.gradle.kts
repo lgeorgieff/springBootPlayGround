640b1340df6b1da85e695da9562d4448e16a90c0
Lukas Georgieff <lcgeorgieff@gmail.com>
2019-11-17 18:50:35 +0100

    spring security: reactive-reactive-oidc-login: Add initial project setup

---
+++ springSecurity5/reactive-oidc-login/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "reactive-oidc-login"
---
+++ springSecurity5/reactive-oidc-login/src/main/kotlin/com/example/oidclogin/controller/MainController.kt
@@ -0,0 +1,15 @@
+package com.example.oidclogin.controller
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.oauth2.core.user.OAuth2User
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+
+@RestController
+class MainController {
+    @GetMapping("/")
+    fun index(@AuthenticationPrincipal oauth2User: Mono<OAuth2User>) = oauth2User
+        .map { obj: OAuth2User -> "Hello ${obj.name}" }
+        .switchIfEmpty(Mono.just("Hello John Doe!"))
+}
